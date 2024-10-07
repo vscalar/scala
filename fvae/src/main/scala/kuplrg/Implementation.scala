ef393d17b457951d25774f657b9043b162d31174
vscalar <vscalar02@gmail.com>
2024-10-07 12:23:45 +0900

    fvae complete

--- fvae/src/main/scala/kuplrg/Implementation.scala
+++ fvae/src/main/scala/kuplrg/Implementation.scala
@@ -1,18 +1,40 @@
 package kuplrg
 
+import javax.swing.text.html.HTML.Tag
+import java.math.BigInteger
+
 object Implementation extends Template {
 
   import Expr.*
   import Value.*
 
+  type BOp[T] = (T, T) => T
+  def numBOp(op: BOp[BigInt]): BOp[Value] = (_, _) match
+    case (NumV(l), NumV(r)) => NumV(op(l, r))
+    case _ => error("invalid operation")
+  
+  val numAdd: BOp[Value] = numBOp(_ + _)
+  val numMul: BOp[Value] = numBOp(_ * _)
+  
   def interp(expr: Expr, env: Env): Value = expr match
-    case Num(n) => n
-    case Add(l,r) => interp(l, env) + interp(r, env = )
-    case Mul(l,r) => interp(l, env) * interp(r, env)
+    case Num(n) => NumV(n)
+    case Add(l,r) => numAdd(interp(l, env), interp(r, env))
+    case Mul(l,r) => numMul(interp(l, env), interp(r, env))
     case Val(x, e, b) => interp(b, env + (x -> interp(e, env)))
     case Id(x) => env.getOrElse(x, error("free identifier"))
-    case App(f, e) => 
-    case Fun(p, e)
+    case App(f, e) => interp(f, env) match
+      case CloV(p, b, fenv) => interp(b, fenv + (p -> interp(e, env)))
+      case _ => error("not a function")
+    case Fun(p, e) => CloV(p, e, env)
 
-  def interpDS(expr: Expr, env: Env): Value = ???
+  def interpDS(expr: Expr, env: Env): Value = expr match
+    case Num(n) => NumV(n)
+    case Add(l,r) => numAdd(interpDS(l, env), interpDS(r, env))
+    case Mul(l,r) => numMul(interpDS(l, env), interpDS(r, env))
+    case Val(x, e, b) => interpDS(b, env + (x -> interpDS(e, env)))
+    case Id(x) => env.getOrElse(x, error("free identifier"))
+    case App(f, e) => interpDS(f, env) match
+      case CloV(p, b, fenv) => interpDS(b, env + (p -> interpDS(e, env)))
+      case _ => error("not a function")
+    case Fun(p, e) => CloV(p, e, env)
 }
